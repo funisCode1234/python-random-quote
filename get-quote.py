021d8f7988d64b7a024f5830d1f205af0d186fc0
john lee <27mac@John-Lees-iMac.local>
2020-11-21 15:41:16 -0800

    Prints first line of quotes

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def Isaac():
-  print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
 
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  print(quotes[0])
 
 if __name__== "__main__":
   Isaac()
